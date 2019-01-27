594e96c76f61284acf13b107cc5c5700940d1783
lolsaveme06 <lolsaveme06@gmail.com>
2019-01-27 10:31:21 +0200

    Add anykernel2

---
+++ Anykernel2/anykernel.sh
@@ -0,0 +1,34 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Green kernel by pqha (alias: lolsaveme06, lolfhd22)
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=whyred
+device.name2=wayne
+device.name3=
+device.name4=
+device.name5=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel install
+dump_boot;
+write_boot;
+
+## end install
+
